4b5b9410e05cdf5e204e9855cfea402a4bf66e07
Tapu Mondal <tapumondal25@gmail.com>
2026-03-15 04:15:48 +0530

    Refactor puzzle game with sound effects and grid size
    Sript for version -1.1

--- script.js
+++ script.js
@@ -1,50 +1,91 @@
+let moveSound = new Audio("sound1.mp3")
+let winSound = new Audio("sound2.mp3")
+
 let board = document.getElementById("board")
 
 let currentImage = ""
+let gridSize = 4
+
 let dragItem = null
 
+
+
 function showImages() {
+
     document.getElementById("menu").style.display = "none"
     document.getElementById("images").style.display = "block"
+
 }
 
+
+
+
+
+
+
+
+
 function backMenu() {
+
     document.getElementById("menu").style.display = "block"
     document.getElementById("images").style.display = "none"
+    document.getElementById("sizeSelect").style.display = "none"
+    document.getElementById("game").style.display = "none"
+
 }
+
+
 
 function startGame(img) {
 
     currentImage = img
 
     document.getElementById("images").style.display = "none"
+    document.getElementById("sizeSelect").style.display = "block"
+
+}
+
+
+
+function startPuzzle(size) {
+
+    gridSize = size
+
+    document.getElementById("sizeSelect").style.display = "none"
     document.getElementById("game").style.display = "block"
 
     createPuzzle()
 
 }
 
+
+
 function createPuzzle() {
 
     board.innerHTML = ""
 
-    let size = 100
+    board.style.gridTemplateColumns = `repeat(${gridSize},1fr)`
+    board.style.gridTemplateRows = `repeat(${gridSize},1fr)`
+
+    let size = 420 / gridSize
+
     let pieces = []
 
     let id = 0
 
-    for (let y = 0; y < 4; y++) {
+    for (let y = 0; y < gridSize; y++) {
 
-        for (let x = 0; x < 4; x++) {
+        for (let x = 0; x < gridSize; x++) {
 
             let piece = document.createElement("div")
 
             piece.className = "piece"
 
             piece.style.backgroundImage = `url(${currentImage})`
+
             piece.style.backgroundPosition = `-${x * size}px -${y * size}px`
 
-            piece.setAttribute("data-id", id)
+            piece.dataset.id = id
 
             piece.draggable = true
 
@@ -66,6 +107,8 @@ function createPuzzle() {
 
 }
 
+
+
 function shuffle(arr) {
 
     for (let i = arr.length - 1; i > 0; i--) {
@@ -73,35 +116,56 @@ function shuffle(arr) {
         let j = Math.floor(Math.random() * (i + 1))
 
         let temp = arr[i]
+
         arr[i] = arr[j]
+
         arr[j] = temp
 
     }
 
 }
 
+
+
 function dragStart() {
+
     dragItem = this
+
 }
 
+
+
 function dragOver(e) {
+
     e.preventDefault()
+
 }
+
+
 
 function drop() {
 
+    // move sound
+    moveSound.currentTime = 0
+    moveSound.play()
+
     let tempImg = this.style.backgroundPosition
+
     let tempId = this.dataset.id
 
     this.style.backgroundPosition = dragItem.style.backgroundPosition
+
     this.dataset.id = dragItem.dataset.id
 
     dragItem.style.backgroundPosition = tempImg
+
     dragItem.dataset.id = tempId
 
     checkWin()
 
 }
+
+
 
 function checkWin() {
 
@@ -112,7 +176,9 @@ function checkWin() {
     pieces.forEach((p, index) => {
 
         if (parseInt(p.dataset.id) !== index) {
+
             solved = false
+
         }
 
     })
@@ -121,7 +187,27 @@ function checkWin() {
 
         setTimeout(() => {
 
-            alert("SABBAS, TURU LABB <3 TOR SOB MONE ACHE! ")
+            // win sound
+            winSound.currentTime = 0
+            winSound.play()
+
+            Swal.fire({
+
+                html: `
+                <img src="like.gif" style="width:130px;margin-bottom:15px;">
+                <h2>SABBAS</h2>
+                <p>TURU LABB &lt;3! TOR SOB MONE ACHE!!</p>
+                `,
+
+                confirmButtonText: "Huh! I know!",
+                allowOutsideClick: false
+
+            }).then(() => {
+
+                winSound.pause()
+                winSound.currentTime = 0
+
+            })
 
         }, 200)
 
@@ -129,6 +215,10 @@ function checkWin() {
 
 }
 
+
+
 function exitGame() {
+
     location.reload()
+
 }
